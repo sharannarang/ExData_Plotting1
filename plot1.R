7d3e40bd9915357dc1c02a23f597950b047a757c
Sharan Narang <sharan.narang@gmail.com>
2014-05-07 10:09:58 -0700

    Add plot2 code and image.

--- plot1.R
+++ plot1.R
@@ -4,8 +4,11 @@ require(sqldf)
 ## Read the dataset only for the required dates
 power_df <- read.csv2.sql("household_power_consumption.txt", sql = "select * from file where Date = '1/2/2007' or Date = '2/2/2007'")
 
+## Merge Date and Time to create a new datetime object
+power_df$DateTime <- strptime(paste(power_df$Date,power_df$Time), format="%d/%m/%Y %H:%M:%S")
+
 ## Open a PNG graphics device
-png("plot1.png", bg="transparent")
+png("plot1.png")
 
 ## Plot the histogram with correct labels.
 with(power_df, hist(Global_active_power, xlab="Global Active Power (kilowatts)", main="Global Active Power", col="Red", ylim=c(0,1200)))
---
+++ plot2.R
@@ -0,0 +1,19 @@
+## Load the sqldf package
+require(sqldf)
+
+## Read the dataset only for the required dates
+power_df <- read.csv2.sql("household_power_consumption.txt", 
+                          sql = "select * from file where Date = '1/2/2007' or Date = '2/2/2007'",
+                          na.strings="?")
+
+## Merge Date and Time to create a new datetime object
+power_df$DateTime <- strptime(paste(power_df$Date,power_df$Time), format="%d/%m/%Y %H:%M:%S")
+
+## Open a PNG graphics device
+png("plot2.png")
+
+## Create plot with lines
+with(power_df, plot(DateTime,Global_active_power,type="l", xlab="",ylab="Global Active Power (kilowatts)"))
+
+## Close the device
+dev.off()
